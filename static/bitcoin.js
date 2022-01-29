8346bcee3ee70b7e4f8066b3987ba1bc67c278ee
Lekshmi Prabha <lekpix@gmail.com>
2022-01-28 19:36:16 -0800

    Adding .js code

---
+++ static/bitcoin.js
@@ -0,0 +1,21 @@
+// var asset=d3.select()
+
+// var tabledata=d3.select()
+//  for (var  in tabledata){
+//      var row=`<tr>
+//                     <td>${tabledata[i].openprice}</td>
+//                     <td>${tabledata[i].closeprice}</td>
+//                     <td>${tabledata[i].alltimehigh}</td>
+//                     <td>${tabledata[i].alltimelow}</td>
+//                     <td>${tabledata[i].volume}</td>
+//                 </tr>`
+//     var table=${'#table-body'}
+//     table.append(row)
+
+//  }
+// //  https://www.youtube.com/watch?v=0WAqNYGrUGM
+
+var BitVar= 30
+var EthVar=10
+
+d3.select("marquee").text("Top Assets --> BitCoin : $" + BitVar +"K Ethereum : $" + EthVar+"K")
